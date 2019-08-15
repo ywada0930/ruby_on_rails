aeba834fe3295a174662061f93f656c22d28830b
ywada0930 <ywada19840930@gmail.com>
2019-08-15 15:21:12 +0900

    thrid-commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "/" => "home#top"
 
   get "about" => "home#about"
-  get 'posts/index' => "post#index"
+  get 'posts/index' => "posts#index"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
