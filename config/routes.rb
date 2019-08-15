1f2cf499fae36fa2067d685dddc285439c7f9d63
ywada0930 <ywada19840930@gmail.com>
2019-08-15 21:40:01 +0900

    fourth-commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "about" => "home#about"
   get 'posts/index' => "posts#index"
+  get "posts/:id" => "posts#show"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
