a0e1628d9729e06415aaf639d66a72fb5f54c9b3
ywada0930 <ywada19840930@gmail.com>
2019-08-16 09:54:42 +0900

    sixthcommit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,18 @@
 class PostsController < ApplicationController
   def index
-      @posts = Post.all
+      @posts = Post.all.order(created_at: :desc)
   end
   def show
     @post = Post.find_by(id:params[:id])
+  end
+  def new
+
+  end
+
+  #create処理を走らせたあと、投稿一覧ページにリダイレクトを行う
+  def create
+    @post=Post.new(content:params[:content])
+    @post.save
+    redirect_to("/posts/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get "/" => "home#top"
-
-  get "about" => "home#about"
   get 'posts/index' => "posts#index"
+  get 'posts/new' => "posts#new"
   get "posts/:id" => "posts#show"
 
+  post "posts/create" => "posts#create"
+
+  get "/" => "home#top"
+  get "about" => "home#about"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
